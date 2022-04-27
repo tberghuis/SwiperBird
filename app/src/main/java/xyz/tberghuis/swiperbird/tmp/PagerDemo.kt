1a1dcb901f341aecf336d63c6436f806a81bc23c
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-27 13:37:28 +1000

    .

--- app/src/main/java/xyz/tberghuis/swiperbird/tmp/PagerDemo.kt
+++ app/src/main/java/xyz/tberghuis/swiperbird/tmp/PagerDemo.kt
@@ -1,9 +1,25 @@
 package xyz.tberghuis.swiperbird.tmp
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.VerticalPager
+import com.google.accompanist.pager.rememberPagerState
 
+
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun PagerDemo() {
-  Text("hello pager demo")
+
+  val pagerState = rememberPagerState()
+  VerticalPager(count = 10, state = pagerState) { page ->
+    // Our page content
+    Text(
+      text = "Page: $page",
+      modifier = Modifier.fillMaxWidth()
+    )
+  }
+
 }
