ef25a700b3a2979983f5f3d5c92b3c8e5f1ebf68
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-28 06:41:21 +1000

    .

--- app/src/main/java/xyz/tberghuis/swiperbird/composables/PlayerViewContainer.kt
+++ app/src/main/java/xyz/tberghuis/swiperbird/composables/PlayerViewContainer.kt
@@ -1,64 +1,17 @@
 package xyz.tberghuis.swiperbird.composables
 
 import android.view.LayoutInflater
-import android.view.TextureView
 import android.view.ViewGroup
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.viewinterop.AndroidViewBinding
-import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.ui.StyledPlayerControlView
-
-import com.google.android.exoplayer2.ui.StyledPlayerView
-import com.google.android.exoplayer2.util.MimeTypes
 import xyz.tberghuis.swiperbird.databinding.PlayerViewBinding
-import xyz.tberghuis.swiperbird.util.logd
-
-
-@Composable
-fun PlayerViewContainerContainer(videoUrl: String) {
-  val context = LocalContext.current
-
-  // todo array of 3 player and playerview
-
-  val player = remember {
-    ExoPlayer.Builder(context).build().apply {
-      playWhenReady = false
-      repeatMode = Player.REPEAT_MODE_ONE
-    }
-  }
-
-  Box(contentAlignment = Alignment.Center,
-    modifier = Modifier
-      .fillMaxSize()
-      .clickable {
-        if (player.isPlaying) {
-          player.pause()
-        } else {
-          player.play()
-        }
-      }
-  ) {
-    PlayerViewContainer(player, videoUrl)
-  }
-}
 
 @Composable
 fun PlayerViewContainer(player: Player, videoUrl: String) {
   // yes i need to construct a fresh view in factory
-  // factory is stateful
 
   LaunchedEffect(Unit) {
     player.setMediaItem(MediaItem.fromUri(videoUrl))
